3fd33d62be73d05126d34bc7b6525a30e22d00c2
Espoir BADJOSSOU <118775150+19THope@users.noreply.github.com>
2025-04-23 14:40:03 +0000

    reviewing python programming

---
+++ habit_tracker_IU-main/habit_tracker_main/src/python/basics_lesson.py
@@ -0,0 +1,49 @@
+'''# Integer → whole number
+age = 25
+# Float → number with decimals
+temperature = 36.6
+
+# String → text, must be in quotes
+name = "Alice"
+
+# Boolean → True or False value
+is_student = True
+
+# Print values to the terminal
+print("Name:", name)           # Outputs: Name: Alice
+print("Temperature:", temperature)  # Outputs: Temperature: 36.6'''
+
+import math
+print(math.sqrt(16))  # Outputs: 4.0
+
+import datetime
+print(datetime.date.today())  # Outputs: today's date
+
+'''
+def check_age():
+    age = int(input("Enter your age: "))
+
+    if age < 18:
+        print("You are a minor.")
+    elif age == 18:
+        print("You are exactly 18.")
+    else:
+        print("You are an adult.")
+
+check_age() '''
+
+def greet(name):
+    print("Hello,", name)
+
+user_name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+if age < 18:
+        print("You are a minor.")
+elif age == 18:
+        print("You are exactly 18.")
+else:
+    greet(user_name)
+
+
+
+
